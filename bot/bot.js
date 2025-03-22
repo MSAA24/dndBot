8f23a66a44a1bd70fc3a426d239d643935a22c91
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2025-03-22 02:11:32 -0300

    agregado clima

---
+++ bot/bot.js
@@ -0,0 +1,24 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+//ID del canal del msj
+const CHANNEL_ID = '1352871493343907891'; 
+
+client.on("ready", () => {
+
+    // Configuramos el intervalo de 24 horas (24 horas = 24 * 60 * 60 * 1000 ms)
+    setInterval(async () => {
+        try {
+            // Obtén el canal donde quieres enviar el mensaje
+            const channel = await client.channels.fetch(CHANNEL_ID);
+
+            // Envía el mensaje
+            await channel.send('¡Este es un mensaje automatizado cada 24 horas!');
+            console.log('Mensaje enviado');
+        } catch (error) {
+            console.error('Error al enviar el mensaje:', error);
+        }
+    }, 10000); // 24 horas en milisegundos
+});
+
+client.login(process.env.TOKEN);
