a5018c728ac797b51616b9301f0f1edd2d1e30de
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2025-03-22 02:26:53 -0300

    agregado intervalo clima

--- index.js
+++ index.js
@@ -48,5 +48,26 @@ client.on('messageCreate', async (message) => {
     }
 });
 
+const CHANNEL_ID = '1352871493343907891'; 
+
+client.on("ready", () => {
+
+    // Configuramos el intervalo de 24 horas (24 horas = 24 * 60 * 60 * 1000 ms)
+    setInterval(async () => {
+        try {
+            // Obtén el canal donde quieres enviar el mensaje
+            const channel = await client.channels.fetch(CHANNEL_ID);
+
+            // Envía el mensaje
+            await channel.send('!cambiarClima');
+            console.log('Mensaje enviado');
+        } catch (error) {
+            console.error('Error al enviar el mensaje:', error);
+        }
+    }, 10000); // 24 horas en milisegundos
+});
+
+
+
 // Iniciar sesión con el token del bot
 client.login(process.env.TOKEN);
