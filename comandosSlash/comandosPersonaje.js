9629cc3daeb0fc2208a06c3144747607d62ed00e
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2025-03-23 18:53:29 -0300

    pequeño fix

--- comandosSlash/comandosPersonaje.js
+++ comandosSlash/comandosPersonaje.js
@@ -21,14 +21,28 @@ const comandosPersonaje = [
             .addIntegerOption(option => 
                 option.setName('nivel')
                     .setDescription('Nivel del personaje')
-                    .setRequired(true)),
+                    .setRequired(true))
+            .addIntegerOption(option => 
+                option.setName('rango')
+                    .setDescription('Rango del personaje')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('imageUrl')
+                    .setDescription('La imagen del personaje')
+                    .setRequired(true))
+            .addStringOption(option =>
+                option.setName('n20')
+                    .setDescription('El url del personaje')
+                    .setRequired(true)),          
         async execute(interaction) {
             const nombre = interaction.options.getString('nombre');
             const raza = interaction.options.getString('raza');
             const clase = interaction.options.getString('clase');
             const nivel = interaction.options.getInteger('nivel');
-            
-            await crearPersonaje(interaction.user.id, nombre, raza, clase, nivel);
+            const rango = interaction.options.getInteger('rango');
+            const imageUrl = interaction.options.getInteger('imageUrl');
+            const n20 = interaction.options.getInteger('n20');
+            await crearPersonaje(interaction.user.id, nombre, raza, clase, nivel, rango, imageUrl, n20);
             await interaction.reply(`Personaje **${nombre}** creado con éxito!`);
         }
     },
@@ -49,7 +63,15 @@ const comandosPersonaje = [
 
                 if (personaje) {
                     // Si se encuentra el personaje
-                    await interaction.reply(`Información del personaje:\nNombre: ${personaje.characterName}\nRaza: ${personaje.race}\nClase: ${personaje.class}\nNivel: ${personaje.level}\nRango: ${personaje.rank}`);
+                    await interaction.reply(
+                        `Información del personaje:
+                            \nNombre: ${personaje.characterName}
+                            \nRaza: ${personaje.race}
+                            \nClase: ${personaje.class}
+                            \nNivel: ${personaje.level}
+                            \nRango: ${personaje.rank}
+                            \nImagen: ${personaje.imageUrl}
+                            \n20: ${personaje.n20Url}`);
                 } else {
                     // Si no se encuentra el personaje
                     await interaction.reply(`No se encontró un personaje con el nombre **${nombrePersonaje}**.`);
