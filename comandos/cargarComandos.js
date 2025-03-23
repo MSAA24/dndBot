7de6684ec7f7fa5392e399c03d5fd0a11b2c1de6
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2025-03-23 15:15:51 -0300

    cambiado clima

--- comandos/cargarComandos.js
+++ comandos/cargarComandos.js
@@ -8,12 +8,10 @@ function cargarComandos() {
 
     for (const archivo of archivos) {
         const comando = require(path.join(carpetaComandos, archivo));
-        if (Array.isArray(comando)) {
-            // Si el módulo exporta un array, agregamos todos los elementos
-            comandos.push(...comando);
-        } else {
-            // Si es un solo comando, lo agregamos directamente
+        if (comando && comando.data) {
             comandos.push(comando);
+        } else {
+            console.warn(`Comando sin 'data': ${archivo}`);
         }
     }
 
