305261ca6c57ef465fa12ca321738768a3fa175b
MSAA24 <marcos.acuna@estudiantes.unahur.edu.ar>
2025-03-24 00:56:04 -0300

    pequeño fix

--- comandosSlash/comandosMoneda.js
+++ comandosSlash/comandosMoneda.js
@@ -2,51 +2,62 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const {crearMoneda,getMoneda} = require("../controllers/monedaController.js");
 
 const comandosMoneda = [
-    
-        {
-            data: new SlashCommandBuilder()
-                .setName('moneda')
-                .setDescription('Gestiona las monedas de los usuarios.')
-                .addSubcommand(subcommand =>
-                    subcommand
-                        .setName('crear')
-                        .setDescription('Crea una moneda para un usuario.')
-                        .addStringOption(option =>
-                            option.setName('nombre')
-                                .setDescription('El nombre de la moneda')
-                                .setRequired(true))
-                        .addIntegerOption(option =>
-                            option.setName('cantidad')
-                                .setDescription('Cantidad de monedas')
-                                .setRequired(false)))
-                .addSubcommand(subcommand =>
-                    subcommand
-                        .setName('ver')
-                        .setDescription('Ver la cantidad de monedas de un usuario.')
-                        .addStringOption(option =>
-                            option.setName('nombre')
-                                .setDescription('El nombre de la moneda')
-                                .setRequired(true))),
+    {
+        data: new SlashCommandBuilder()
+            .setName('crear_moneda')
+            .setDescription('Crea una moneda para un usuario.')
+            .addStringOption(option =>
+                option.setName('nombre')
+                    .setDescription('El nombre de la moneda')
+                    .setRequired(true))
+            .addIntegerOption(option =>
+                option.setName('cantidad')
+                    .setDescription('Cantidad de monedas')
+                    .setRequired(false)),
         
-            async execute(interaction) {
-                const userId = interaction.user.id;
-                const monedaUser= `${userId}_${nombre}`
-                const subcommand = interaction.options.getSubcommand();
-        
-                if (subcommand === 'crear') {
-                    const nombreMoneda = interaction.options.getString('nombre');
-                    const cantidadMonedas = interaction.options.getInteger('cantidad') || 1;
-        
-                    await crearMoneda(monedaUser, nombreMoneda, cantidadMonedas);
-                    await interaction.reply(`Se ha creado la moneda **${nombreMoneda}** con ${cantidadMonedas} monedas para ti.`);
-        
-                } else if (subcommand === 'ver') {
-                    const nombreMoneda = interaction.options.getString('nombre');
-                    const cantidad = await getMoneda(monedaUser, nombreMoneda);
-                    await interaction.reply(`Tienes ${cantidad} monedas de **${nombreMoneda}**.`);
-                }
+        async execute(interaction) {
+            const userId = interaction.user.id;
+            const characterId = `${userId}_${nombrePersonaje}`;
+            const nombreMoneda = interaction.options.getString('nombre');
+            const cantidadMonedas = interaction.options.getInteger('cantidad') || 1;
+
+            try {
+                await crearMoneda(characterId, nombreMoneda, cantidadMonedas);
+                await interaction.reply(`Se ha creado la moneda **${nombreMoneda}** con ${cantidadMonedas} monedas para ti.`);
+            } catch (error) {
+                console.error("Error creando moneda:", error);
+                await interaction.reply("Hubo un error al crear la moneda. Intenta nuevamente.");
             }
         }
+    },
+    {
+        data: new SlashCommandBuilder()
+            .setName('ver_moneda')
+            .setDescription('Ver la cantidad de monedas de un usuario.')
+            .addStringOption(option =>
+                option.setName('nombre')
+                    .setDescription('El nombre de la moneda')
+                    .setRequired(true)),
+        
+        async execute(interaction) {
+            const userId = interaction.user.id;
+            const characterId = `${userId}_${nombrePersonaje}`;
+            const nombreMoneda = interaction.options.getString('nombre');
+
+            try {
+                const cantidad = await getMoneda(characterId, nombreMoneda);
+                if (cantidad !== null) {
+                    await interaction.reply(`Tienes ${cantidad} monedas de **${nombreMoneda}**.`);
+                } else {
+                    await interaction.reply(`No tienes monedas de **${nombreMoneda}**.`);
+                }
+            } catch (error) {
+                console.error("Error al obtener la moneda:", error);
+                await interaction.reply("Hubo un error al obtener la cantidad de monedas. Intenta nuevamente.");
+            }
+        }
+    }
+        
 ];
 
 module.exports = comandosMoneda;
